1979f2241aa164aba2d257cfbc9dbb1199ed725b
keiono <keiono@gmail.com>
2013-04-08 18:01:18 -0700

    Options had been added to the deploy script.
    Change-Id: Iaa2cb7f009d2a7d2e4520a3fe1c8e4e915904684

--- deploy_installers.sh
+++ deploy_installers.sh
@@ -1,39 +1,81 @@
 #!/bin/sh
+
 ###############################################################################
 #
 # Build & Upload Installers 
 #   Currently this script works only on Kei's Mac.
 #
 ###############################################################################
-if (($# != 1)); then
-	printf "%b" "Usage: deploy_installers.sh branch_name\n" >&2
+
+CMDNAME=$(basename $0)
+
+##########################
+# Options:
+#
+#  -a: Upload API docs
+#  -u: Update API and IMPL bundles
+#
+###########################
+
+ERROR_MESSAGE="Usage: $CMDNAME [-a] [-u] branch_name"
+
+while getopts 'aub:' OPT
+do
+	case $OPT in
+		a)	FLG_A=1 ;;
+		u)	FLG_U=1 ;;
+		?)	echo $ERROR_MESSAGE 1>&2
+				exit 1 ;;
+	esac
+done
+
+if [ "$FLG_A" ]; then
+	echo " - API Upload option is on: JavaDoc will be uploaded later."
+fi
+
+if [ "$FLG_U" ]; then
+	echo " - Update option is on: Build API and IMPL first."
+fi
+
+shift $(($OPTIND - 1))
+
+branch=$*
+
+if [[ -z $branch ]]; then
+	echo "Branch name is required. $ERROR_MESSAGE" 1>&2
 	exit 1
 fi
 
-branch=$1
+echo "Target branch is: $branch"
 
-function checkoutAndBuild {
-	# 1. Clone remote if not available
-	dist=$(ls -alh | grep $targetProject)
-	echo "Directory is: $dist"
-
-	if [[ -z $dist ]]; then
-		echo "$targetProject does not exist.  Cloning..."
-		git clone https://github.com/cytoscape/$targetProject.git
-	fi
-
-	# 2. Build it
-	cd $targetProject 
-
-	# Switch branch
+function build {
 	git checkout $branch || { echo Abort: Could not switch to $branch; exit 1; }
 	git pull || { echo Abort: Failed to pull changes.; exit 1; }
 
 	mvn clean install || { echo Abort: Build Failed; exit 1; }
 }
 
-targetProject="cytoscape-gui-distribution"
-checkoutAndBuild
+###################################
+# Build API and Impl if necessary
+###################################
+
+if [ "$FLG_U" ]; then
+	echo "Building API bundles..."
+	cd ../api
+	build
+	
+	echo "Building IMPL bundles..."
+	cd ../impl
+	build
+fi
+
+
+########################
+# Build distribution
+########################
+echo "Building GUI Distribution..."
+cd ../gui-distribution
+build
 
 cd ./packaging
 mvn clean install || { echo Abort: Installer Build Failed; exit 1; }
@@ -55,16 +97,14 @@ scp ./*.zip $server || { echo Abort: Could not upload zipped file.; exit 1; }
 scp ./*.sh $server || { echo Abort: Could not upload UNIX installer.; exit 1; }
 
 
-# API Bundles
-targetProject="cytoscape-app-developer"
-cd ../../../../
-checkoutAndBuild
-cd targetProject
-ls
-cd ./target
-scp -r ./API $server || { echo Abort: Could not upload JavaDoc files.; exit 1; }
+if [ "$FLG_A" ]; then
+	# API Bundles
+	cd ../../../../app-developer/
+	build
+	cd ./target
+	scp -r ./API $server || { echo Abort: Could not upload JavaDoc files.; exit 1; }
+fi
 
 deployURL="http://chianti.ucsd.edu/~kono/data/cy3latest/$deployDir"
-
 echo "Finished.  Visit $deployURL"
 open $deployURL
